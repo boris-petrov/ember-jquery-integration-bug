46b0f27d011f014b7f99c6a6cfeb1e8503417f74
Boris Petrov <boris_petrov@live.com>
2019-10-08 00:15:17 +0300

    Showcase bug

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    'ember-cli-babel': {
+      includeExternalHelpers: true,
+      includePolyfill: true,
+    },
+
     // Add options here
   });
 
